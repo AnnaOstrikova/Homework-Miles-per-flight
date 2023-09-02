b17267422b21982c72d7f3c768bdc53df1c9a4d8
Anna <pomozovaanna@yandex.ru>
2023-09-02 18:59:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int oneBonus = 20;
+        int bonus = price / oneBonus;
+        System.out.println("Количество бонусных миль - " + bonus);
+    }
+}
